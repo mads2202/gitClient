61aa79cbd6ebd380ec1b5e2b9dac219c3ff9abe8
dmalyshev <dmalyshev@avaya.com>
2021-11-16 16:15:38 +0300

    implement login via rx

--- app/src/main/java/com/mads2202/gitclient/presenters/LoginPresenterImpl.kt
+++ app/src/main/java/com/mads2202/gitclient/presenters/LoginPresenterImpl.kt
@@ -2,16 +2,31 @@ package com.mads2202.gitclient.presenters
 
 import com.github.terrakok.cicerone.Router
 import com.mads2202.gitclient.domen.GitUser
+import com.mads2202.gitclient.domen.GitUserDao
+import com.mads2202.gitclient.domen.GitUserDaoImpl
 import com.mads2202.gitclient.presenters.LoginContract.*
 import com.mads2202.gitclient.ui.Screens
 import com.mads2202.gitclient.ui.ViewState
 import com.mads2202.gitclient.util.regExMailPattern
 import com.mads2202.gitclient.util.regExPasswordPattern
+import io.reactivex.Observable
+import io.reactivex.disposables.CompositeDisposable
 import java.util.regex.Matcher
 import java.util.regex.Pattern
 
 
 class LoginPresenterImpl(val router: Router) : LoginPresenter() {
+    lateinit var compositeDisposable: CompositeDisposable
+    private val userRepo: GitUserDao = GitUserDaoImpl()
+    override fun onFirstViewAttach() {
+        compositeDisposable = CompositeDisposable()
+        super.onFirstViewAttach()
+    }
+
+    override fun onDestroy() {
+        compositeDisposable.dispose()
+        super.onDestroy()
+    }
 
     override fun validateEmail(email: String) {
         if (!isValidString(Pattern.compile(regExMailPattern), email)) {
@@ -27,7 +42,7 @@ class LoginPresenterImpl(val router: Router) : LoginPresenter() {
 
     override fun onLogin(email: String, password: String) {
         if (isValidCredentials(email, password)) {
-            viewState.rememberCredentials(email,password)
+            viewState.rememberCredentials(email, password)
             router.navigateTo(Screens.openMainScreen())
             viewState.setState(ViewState.SUCCESS)
         }
@@ -35,14 +50,16 @@ class LoginPresenterImpl(val router: Router) : LoginPresenter() {
     }
 
     private fun isValidCredentials(email: String, password: String): Boolean {
-        var user: GitUser? = null
-        GitUserDao.gitUserList.forEach {
-            if (it.email == email) {
-                user = it
-                return@forEach
-            }
-        }
-        return user?.password == password
+
+        var isValid = false
+        compositeDisposable.add(
+            userRepo.getUsers()
+                .flatMap { users -> Observable.fromIterable(users) }
+                .filter { user -> user.email == email }
+                .subscribe { user ->
+                    isValid = user.password == password
+                })
+        return isValid
     }
 
     override fun onForgetPassword() {
